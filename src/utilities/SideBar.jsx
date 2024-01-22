62e45200d43db33adb7134b9e526729d39ea800a
WebHav0cx <unknownme403@proton.me>
2024-01-22 01:08:37 +0100

    updated sidebar

--- src/utilities/SideBar.jsx
+++ src/utilities/SideBar.jsx
@@ -5,34 +5,43 @@ import { Link } from "react-router-dom";
 
 function SideBar() {
   return (
-    <div className="flex flex-col justify-center items-center gap-10 pt-10 border border-gray-300 w-56 rounded-tr-[45px] rounded-br-[45px] font-sans mr-4">
-      <div className="flex gap-6 justify-center items-center font-bold text-2xl px-10">
+    <div className="flex flex-col justify-center items-center gap-10 pt-10 px-8  border border-gray-300 rounded-tr-[45px] rounded-br-[45px] font-sans">
+      <div className="flex gap-6 justify-center items-center font-dm-sans font-bold text-2xl ">
         <img src={Tv} alt="Movie Box Logo" />
         <p>MovieBox</p>
       </div>
-      <ul className="flex flex-col gap-6 justify-center items-center ">
-        <Link to="/" className="flex gap-4 justify-center items-center">
-          <Icon className=" text-gray-400" icon="cil:home" />
-          <p className="font-semibold ">Home</p>
+      <ul className="flex flex-col gap-6  text-xl text-gray-400 font-sans font-semibold ">
+        <Link to="/" className="flex gap-3">
+          <Icon className="w-[25px] h-[25px] text-gray-400" icon="cil:home" />
+          <p className=" ">Home</p>
         </Link>
         <Link
           to="#"
-          className="flex gap-4 justify-center items-center border-r-4 py-4 border-r-rose-800 bg-rose-300"
+          className="flex gap-3 border-r-4 py-11 w-full border-r-rose-800 bg-rose-300"
         >
-          <Icon className=" text-black" icon="bx:camera-movie" />
-          <p className="font-semibold  text-rose-800">Movies</p>
+          <Icon
+            className="w-[25px] h-[25px] text-black"
+            icon="bx:camera-movie"
+          />
+          <p className=" text-rose-800">Movies</p>
         </Link>
-        <Link to="#" className="flex gap-4 justify-center items-center ">
-          <Icon className=" text-gray-400" icon="arcticons:youtube-tv" />
-          <p className="font-semibold ">TV Series</p>
+        <Link to="#" className="flex gap-3">
+          <Icon
+            className="w-[25px] h-[25px] text-gray-400"
+            icon="arcticons:youtube-tv"
+          />
+          <p className="">TV Series</p>
         </Link>
-        <Link to="#" className="flex gap-4 justify-center items-center">
-          <Icon className=" text-gray-400" icon="fluent:calendar-20-regular" />
-          <p className="font-semibold ">Upcoming</p>
+        <Link to="#" className="flex gap-3">
+          <Icon
+            className="w-[25px] h-[25px] text-gray-400"
+            icon="fluent:calendar-20-regular"
+          />
+          <p className="">Upcoming</p>
         </Link>
       </ul>
 
-      <div className="border rounded-lg border-rose-600 pt-8 flex flex-col justify-center items-center px-4 w-44 gap-4 pb-4 bg-rose-50">
+      <div className="border rounded-lg border-rose-600 pt-8 flex flex-col justify-center items-center px-4 w-44 text-gray-600 gap-4 pb-4 bg-rose-50">
         <p className="font-semibold text-base">
           Play movie quizes and earn free tickets
         </p>
